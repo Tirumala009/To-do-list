0afb818c7802ad4bb20c322a8c3dc549761b2828
Tirumala009 <69470807+Tirumala009@users.noreply.github.com>
2020-08-25 18:24:17 +0100

    Updates

---
+++ precache-manifest.e7e854a2c3f52c46dc3cf984e1fc28d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887fc8f3fb627a424f303b2e95996520",
+    "url": "/to-do%20list/index.html"
+  },
+  {
+    "revision": "c6998652ab8f500dafa7",
+    "url": "/to-do%20list/static/css/main.6efab03d.chunk.css"
+  },
+  {
+    "revision": "6c7de1d697aea276394c",
+    "url": "/to-do%20list/static/js/2.bff7c8db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do%20list/static/js/2.bff7c8db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6998652ab8f500dafa7",
+    "url": "/to-do%20list/static/js/main.52bd45ad.chunk.js"
+  },
+  {
+    "revision": "f3450a1f3ff8e42241ff",
+    "url": "/to-do%20list/static/js/runtime-main.e890f519.js"
+  }
+]);
